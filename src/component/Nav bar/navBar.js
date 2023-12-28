2285825b4bbac6b9381cd5ba6e73533703b8531f
AMS <111883236+AMS003010@users.noreply.github.com>
2023-12-28 19:09:05 +0530

    commit

--- src/component/Nav bar/navBar.js
+++ src/component/Nav bar/navBar.js
@@ -3,18 +3,46 @@ import './index.css'
 import Logo from "../logo/logo";
 
 const NavBar = () => {
-    return(
-        <div>
-            <div  className='navWrapper'>
-                <Logo/>
-                <div className='logoDiv'>
-                    <span>Home</span>
-                    <span>Projects</span>
-                    <span>About me</span>
-                </div>
-            </div>            
+    const updateLogoContent = () => {
+      const logoDiv = document.querySelector('.logoDiv');
+      if (logoDiv) {
+        if (window.innerWidth <= 450) {
+          // Remove existing child nodes from logoDiv
+          while (logoDiv.firstChild) {
+            logoDiv.removeChild(logoDiv.firstChild);
+          }
+  
+          // Create and append a new empty div with the class "logoDivChild"
+          const node = document.createElement("div");
+          node.className = "logoDivChild";
+          logoDiv.appendChild(node);
+        } else {
+          // Reset the content if needed
+          // For example, you can add back the span elements
+          logoDiv.innerHTML = '<span>Home</span><span>Projects</span><span>About me</span>';
+        }
+      }
+    };
+  
+    // Initial check on page load
+    updateLogoContent();
+  
+    // Add event listener for window resize
+    window.addEventListener('resize', updateLogoContent);
+  
+    return (
+      <div>
+        <div className='navWrapper'>
+          <Logo />
+          <div className='logoDiv'>
+            <span>Home</span>
+            <span>Projects</span>
+            <span>About me</span>
+          </div>
         </div>
-    )
-}
-
-export default NavBar;
+      </div>
+    );
+  }
+  
+  export default NavBar;
+  
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,47 @@
 import NavBar from "../component/Nav bar/navBar";
 
+import githubIcon from '../component/img/github.png';
+import linkedinIcon from '../component/img/linkedin.png';
+import resumeIcon from '../component/img/resume.png';
+
 const Home = () => {
+
+    const myStyle = {
+        height:'100vh',
+        padding:'0px',
+        margin:'0px'
+    }
+
+    const divStyle = {
+        display: 'flex',
+        marginLeft:'3.3rem',
+        marginRight:'3.3rem'
+    }
+
     return(
-        <div style={{height:'100vh',padding:'0px',margin:'0px'}}>
+        <div style={myStyle}>
             <NavBar />
+            <div className='introHomeContainer' style={divStyle}>
+                <div className="introHome">
+                    Hi there!<br/>
+                    I'm <span className="homeName">Abhijith M S</span><br/>
+                    <div className="homeDesc">
+                        <div>Passionate&nbsp;<span>.</span>&nbsp;Developer&nbsp;<span>.</span>&nbsp;Innovator</div><br/>
+                        Dedicated and Creative with a Flair for Transforming Ideas into Technological Marvels. Currently pursuing my BTech in Computer Science at PES University in Bengaluru. 
+                    </div>
+                    <div style={{display:'flex'}}>
+                        <a href='https://www.linkedin.com/in/abhijith-m-s-221855275/' target='blank' className="socialHomeLink">
+                            <img src={resumeIcon} alt='l' className='socialHomeIcon'/>&nbsp;My Resume
+                        </a>
+                        <a href='https://www.linkedin.com/in/abhijith-m-s-221855275/' target='blank' className="socialHomeLink">
+                            <img src={linkedinIcon} alt='l' className='socialHomeIcon'/>&nbsp;Linkedin
+                        </a>
+                        <a href='https://github.com/AMS003010' target='blank' className="socialHomeLink">
+                            <img src={githubIcon} alt='l' className='socialHomeIcon'/>&nbsp;Github
+                        </a>
+                    </div>                   
+                </div>
+            </div>
         </div>
     )
 }
