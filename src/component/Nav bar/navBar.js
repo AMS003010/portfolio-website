db94eb2c4ce0cb8c1af99daeda16a67286b9b112
AMS <111883236+AMS003010@users.noreply.github.com>
2024-01-02 17:14:18 +0530

    commit

--- src/component/Nav bar/navBar.js
+++ src/component/Nav bar/navBar.js
@@ -5,33 +5,6 @@ import { NavLink } from 'react-router-dom';
 import Logo from "../logo/logo";
 
 const NavBar = () => {
-    const updateLogoContent = () => {
-      const logoDiv = document.querySelector('.logoDiv');
-      if (logoDiv) {
-        if (window.innerWidth <= 450) {
-          // Remove existing child nodes from logoDiv
-          while (logoDiv.firstChild) {
-            logoDiv.removeChild(logoDiv.firstChild);
-          }
-  
-          // Create and append a new empty div with the class "logoDivChild"
-          const node = document.createElement("div");
-          node.className = "logoDivChild";
-          logoDiv.appendChild(node);
-        } else {
-          // Reset the content if needed
-          // For example, you can add back the span elements
-          logoDiv.innerHTML = '<span>Home</span><span>Projects</span><span>About me</span>';
-        }
-      }
-    };
-  
-    // Initial check on page load
-    updateLogoContent();
-  
-    // Add event listener for window resize
-    window.addEventListener('resize', updateLogoContent);
-  
     return (
       <div>
         <div className='navWrapper'>
@@ -47,4 +20,3 @@ const NavBar = () => {
   }
   
   export default NavBar;
-  
